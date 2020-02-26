9054a9aac2d59f726aea47f519b4ad147c30889f
shantiscript <sebastian.grede@gmail.com>
2020-02-26 15:15:38 +0800

    Update fluidsynth.py

--- fluidsynth.py
+++ fluidsynth.py
@@ -420,8 +420,7 @@ class Synth:
         self.router = None
     def setting(self, opt, val):
         """change an arbitrary synth setting, type-smart"""
-        opt = opt.encode()
-        if isinstance(val, basestring):
+        if isinstance(val, str):
             fluid_settings_setstr(self.settings, opt, val)
         elif isinstance(val, int):
             fluid_settings_setint(self.settings, opt, val)
